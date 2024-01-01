91a351c33dcfcb6a667c5547f7e3457e293b950b
Jacques <jacquesvanluyck@gmail.com>
2024-01-01 22:49:40 +0100

    update

--- src/app/contact/contact.service.ts
+++ src/app/contact/contact.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
-  url: string = 'https://bluewave-tech.de/sendMail.php';
+  url: string = environment.contactformPath;
 
   constructor(private httpClient: HttpClient) { }
-
-
 
   sendAnEmail(data: object): Observable<{}> {
     const headers = new HttpHeaders().append('Content-Type', 'application/JSON');
