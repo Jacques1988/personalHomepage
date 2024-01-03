868c1449cdf46652b5752b7344fd9b5f81f13188
Jacques <jacquesvanluyck@gmail.com>
2024-01-03 15:04:47 +0100

    update header with balls in background

---
+++ src/app/header/teaser/welcome-teaser/background.ts
@@ -0,0 +1,23 @@
+import { DodecahedronGeometry, MeshStandardMaterial, Mesh } from "three";
+
+export class HeaderBackground {
+    backGroundGeometry = new DodecahedronGeometry(0.75, 3);
+    backGroundMaterial = new MeshStandardMaterial({
+        color: 0x8121cf,
+    });
+    background = new Mesh(this.backGroundGeometry, this.backGroundMaterial);
+    elementCount = 20;
+
+    buildBackground() {
+        if (window.innerWidth < 500) {
+            this.elementCount = 10;
+        }
+        const randomPositionX = (Math.random() - 0.5) * (window.innerWidth / this.elementCount);
+        const randomPositionY = (Math.random() - 0.5) * (window.innerHeight / this.elementCount);
+        const randomPositionZ = -Math.random() * 25;
+        this.background.position.set(randomPositionX, randomPositionY, randomPositionZ);
+        return this.background;
+    }
+
+
+}
--- src/app/header/teaser/welcome-teaser/settings.ts
+++ src/app/header/teaser/welcome-teaser/settings.ts
@@ -1,12 +1,14 @@
-import { Scene, DirectionalLight, PerspectiveCamera, WebGLRenderer, } from 'three'
+import { Scene, DirectionalLight, PerspectiveCamera, WebGLRenderer, Clock } from 'three'
 import { sizes } from './sizes';
 import { Ring } from './Ring';
+import { HeaderBackground } from './background';
 import { StyleLoader } from './StyleLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 export class WelcomeTeaser {
   styleLoader: StyleLoader = new StyleLoader();
   renderer: WebGLRenderer;
+  clock: Clock = new Clock();
   sizes: sizes;
   camera: PerspectiveCamera;
   scene: Scene = new Scene();
@@ -15,6 +17,10 @@ export class WelcomeTeaser {
   ringComponent: Ring = new Ring(this.scene, this.profileImage);
   ringSurface: string[] = this.styleLoader.loadRingTextures();
   ring = this.ringComponent.buildRing(this.ringSurface);
+  headerBackGround: any;
+  backGroundElements = {
+    count: 100,
+  }
   controls: OrbitControls;
 
   constructor(private canvas: HTMLCanvasElement, private canvasContainer: HTMLElement) {
@@ -26,7 +32,7 @@ export class WelcomeTeaser {
       height: this.canvasContainer.clientHeight,
       aspect: this.canvasContainer.clientWidth / this.canvasContainer.clientHeight,
     }
-    this.camera = new PerspectiveCamera(75, this.sizes.aspect, 0.01, 1000);
+    this.camera = new PerspectiveCamera(20, this.sizes.aspect, 1, 100);
     this.renderer = new WebGLRenderer({
       canvas: this.canvas,
       alpha: true,
@@ -39,11 +45,10 @@ export class WelcomeTeaser {
       this.sizes.height = this.canvasContainer.clientHeight;
       this.sizes.aspect = this.canvasContainer.clientWidth / this.canvasContainer.clientHeight;
       this.camera.aspect = this.sizes.aspect;
-
       this.updateCamera();
       this.rendererUpdates();
     })
-
+    this.buildBackground();
     this.settings();
     this.scene.add(
       this.camera,
@@ -52,13 +57,25 @@ export class WelcomeTeaser {
     );
   }
 
+  buildBackground() {
+    for (let i = 0; i < this.backGroundElements.count; i++) {
+      const headerBackground = new HeaderBackground();
+      this.headerBackGround = headerBackground.buildBackground();
+      this.scene.add(this.headerBackGround);
+    }
+  }
+
+  animateBackground(time: number) {
+    this.headerBackGround.rotation.z = time * 0.5;
+  }
+
   settings() {
 
     if (window.innerWidth < 500) {
-      this.camera.position.z = 18;
+      this.camera.position.z = 60;
       this.ring.position.y = 3;
     } else {
-      this.camera.position.z = 10;
+      this.camera.position.z = 40;
     }
 
     this.canvas.width = this.sizes.width;
@@ -68,14 +85,27 @@ export class WelcomeTeaser {
       this.sizes.width,
       this.sizes.height
     );
-
+    this.controlsSettings();
     this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
     this.startScreen();
   }
 
+  controlsSettings() {
+    this.controls.enableDamping = true;
+    this.controls.dampingFactor = 0.03;
+    this.controls.enableZoom = false;
+    this.controls.minPolarAngle = Math.PI / 4;
+    this.controls.maxPolarAngle = Math.PI / 1.25;
+    this.controls.maxAzimuthAngle = Math.PI * 0.25;
+    this.controls.minAzimuthAngle = -Math.PI * 0.25;
+  }
+
   startScreen = () => {
+    const elapsedTime = this.clock.getElapsedTime();
+    this.animateBackground(elapsedTime);
+    this.controls.update();
     this.renderer.render(this.scene, this.camera);
-    requestAnimationFrame(this.startScreen)
+    requestAnimationFrame(this.startScreen);
   }
 
   rendererUpdates() {
